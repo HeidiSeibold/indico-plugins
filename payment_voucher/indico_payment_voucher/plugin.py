7a610d46dc3cae304d13a38febe8fad5a9c3b1f6
HeidiSeibold <heidi.bold@gmail.com>
2025-10-04 12:03:16 +0200

    remove self.render_template

--- payment_voucher/indico_payment_voucher/plugin.py
+++ payment_voucher/indico_payment_voucher/plugin.py
@@ -124,17 +124,7 @@ class VoucherPaymentPlugin(PaymentPluginMixin, IndicoPlugin):
     
     def get_blueprints(self):
         return blueprint
-    
-    def render_payment_form(self, registration):
-        form = VoucherForm()
-        return self.render_template(
-            'event_payment_form.html',
-            registration=registration,
-            form=form
-        )
 
-
-    
     def adjust_payment_form_data(self, data):
         """Add payment route to form data"""
         registration = data['registration']
